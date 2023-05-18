9a3d3b8dd7e7fc0fef6035c3565d1ec6b442d1d4
Yani <yanivlascogmail.com>
2023-05-18 15:50:44 +0200

    link add to head

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+      <link href="https://api.fontshare.com/v2/css?f[]=supreme@800,400,200,300,500,700&f[]=technor@300,600,400,700,500&display=swap" rel="stylesheet" />
+      </Head>
       <body>
         <Main />
         <NextScript />
