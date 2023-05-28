8066e333f38f3071b906cec1a9802efbb2266cff
Ali Merkouk <63125615+Sava-Jcript@users.noreply.github.com>
2023-05-28 17:52:33 +0200

    changes

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,11 @@ import React from 'react';
 export default function Home() {
   const router = useRouter();
 
-  // Redirect to "/schedule" page
+
   React.useEffect(() => {
     router.push('/bands');
   }, []);
 
-  // Render null or a loading indicator while redirecting
+
   return null;
 }
