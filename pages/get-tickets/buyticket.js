5a525847830def86e4019a45b153f2872d4f4f96
Ali Merkouk <63125615+Sava-Jcript@users.noreply.github.com>
2023-05-29 05:37:07 +0200

    changes

--- pages/get-tickets/buyticket.js
+++ pages/get-tickets/buyticket.js
@@ -2,6 +2,21 @@ import React from "react";
 import { useRouter } from "next/router";
 
 
+
+
+
+const router =useRouter();
+const availableSpotArray=props.availableSpotData;
+
+
+
+
+
+
+
+
+
+
 // Fetch data
 export async function getServerSideProps (){
     const ret=await fetch("https://bittersweet-painted-willow.glitch.me/available-spots"); 
@@ -14,3 +29,5 @@ export async function getServerSideProps (){
     };
 
 }
+
+
