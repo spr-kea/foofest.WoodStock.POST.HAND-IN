c4fc84e26e50a908b753fee1d5de0a183847328a
Ali Merkouk <63125615+Sava-Jcript@users.noreply.github.com>
2023-05-30 01:03:20 +0200

    new day new branch

--- pages/tickets/index.js
+++ pages/tickets/index.js
@@ -1,32 +1,30 @@
 import React from "react";
 import { useRouter } from "next/router";
 
-function Step1(props) {
-  const areaArray = props.availableSpotData;
+function firstStepBooking(props) {
+  const availableSpotArray = props.availableSpotData;
   const router = useRouter();
-  let ticketsValid = props.orderInfo.totalTickets > 0;
-
-  async function validateAndReserve() {
-    const id = await reserveSpot(props.orderInfo.selectedArea, props.orderInfo.totalTickets);
-    await validateArea(id);
+  async function confirmBooking() {
+    const id = await reserveCampingSpot(props.bookingInfos.selectedArea);
+    await spotAreaValid(id);
   }
 
-  async function validateArea(id) {
-    const area = props.orderInfo.selectedArea;
-    const index = areaArray.findIndex((item) => item.area === area);
-    const available = areaArray[index]?.available || 0;
+  async function spotAreaValid(id) {
+    const area = props.bookingInfos.selectedArea;
+    const index = availableSpotArray.findIndex((item) => item.area === area);
+    const available = availableSpotArray[index]?.available || 0;
 
-    if (props.orderInfo.totalTickets <= available) {
-      props.setOrderInfo({ ...props.orderInfo, validates: true, orderID: id });
-      console.log("area validation: all good");
-      router.push("/tickets/step2");
-    } else {
-      props.setOrderInfo({ ...props.orderInfo, validates: false });
-      console.log("area validation: no bueno");
+    if (props.bookingInfos.totalTickets <= available) {
+      props.setbookingInfos({ ...props.bookingInfos, validates: true, orderID: id });
+
+      router.push("/tickets/bookingStep2");
+
+      props.setbookingInfos({ ...props.bookingInfos, validates: false });
+
     }
   }
 
-  async function reserveSpot(chosenArea, chosenAmount) {
+  async function reserveCampingSpot(chosenArea, chosenAmount) {
     const request = await fetch("https://bittersweet-painted-willow.glitch.me/reserve-spot", {
       method: "PUT",
       headers: {
@@ -41,52 +39,56 @@ function Step1(props) {
     return id;
   }
 
-  function goBack() {
+  function cancelBooking() {
     router.push("/");
   }
 
   return (
-    <div className="order-container">
+    <div >
       <h2>Select your tickets</h2>
-      <InputCounter updateRegTickets={props.updateRegTickets} title={"Regular"} name={"RegTicket"} subtitle={"All days"} description={"Access to concerts and vendors."} price={"799,-"} setTickets={props.setTickets} />
-      <InputCounter updateVIPTickets={props.updateVIPTickets} title={"VIP"} name={"VIPTicket"} subtitle={"All days"} description={"Includes food & drink discounts and early access to stages."} price={"1299,-"} />
-      <h2>Select your camp area</h2>
-      <InputDrop
-        selectArea={props.selectArea}
-        optionOne={areaArray[0]?.area}
-        optionOneSpace={areaArray[0]?.available}
-        optionTwo={areaArray[1]?.area}
-        optionTwoSpace={areaArray[1]?.available}
-        optionThree={areaArray[2]?.area}
-        optionThreeSpace={areaArray[2]?.available}
-        optionFour={areaArray[3]?.area}
-        optionFourSpace={areaArray[3]?.available}
-        optionFive={areaArray[4]?.area}
-        optionFiveSpace={areaArray[4]?.available}
+      <input updateRegularTickets={props.updateRegularTickets} title={"Regular"} name={"RegularTicket"} price={"799,-"} setTickets={props.setTickets} />
+      <InputCounter updateVIPTickets={props.updateVIPTickets} title={"VIP"} name={"VIPTicket"} price={"1299,-"} />
+      <h2>Select your camping area</h2>
+      <input
+        selectedSpot={props.selectedSpot}
+        selectOption1={availableSpotArray[0]?.area}
+        selectOption1Space={availableSpotArray[0]?.available}
+        selectOption2={availableSpotArray[1]?.area}
+        selectOption2Space={availableSpotArray[1]?.available}
+        selectOption3={availableSpotArray[2]?.area}
+        selectOption3Space={availableSpotArray[2]?.available}
+        selectOption4={availableSpotArray[3]?.area}
+        selectOption4Space={availableSpotArray[3]?.available}
+        selectOption5={availableSpotArray[4]?.area}
+        selectOption5Space={availableSpotArray[4]?.available}
       />
-      {!ticketsValid && <p className="feedback">Please add at least one ticket to your order.</p>}
-      {props.orderInfo.validates === false && <p className="feedback">Please select an area with enough space for the amount of tickets.</p>}
-      <div className="booking-steps-buttons">
-        <button className="secondary" onClick={goBack}>
+      
+      <div>
+        <button  onClick={cancelBooking}>
           Cancel
         </button>
-        <button className="primary" onClick={validateAndReserve}>
-          Select camp options →
+        <button onClick={confirmBooking}>
+          Select Camping Options 
         </button>
       </div>
     </div>
   );
 }
 
-export default Step1;
+
+export default firstStepBooking;
+
 
 // Fetch data
-export async function getServerSideProps() {
-  const response = await fetch("https://bittersweet-painted-willow.glitch.me/available-spots");
-  const availableSpotData = await response.json();
-  console.log(availableSpotData);
+export async function getServerSideProps (){
+    const res=await fetch("https://bittersweet-painted-willow.glitch.me/available-spots"); 
+    const availableSpotData=await res.json(); 
 
-  return {
-    props: { availableSpotData },
-  };
+
+    return {
+        props:{availableSpotData},
+
+    };
+
 }
+
