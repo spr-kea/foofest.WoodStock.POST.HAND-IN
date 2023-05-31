5f576581f23b84631ab1eda89ba9ee6837cdce97
Yani <yanivlascogmail.com>
2023-05-31 18:17:18 +0200

    navBar fully responsive tailwind

--- components/nav-bar/NavBar.jsx
+++ components/nav-bar/NavBar.jsx
@@ -9,15 +9,15 @@ function NavBar() {
   return (
     <section className="bg-primary-color2 flex justify-center fixed top-0 w-full h-20 z-10 font-mono">
       <div className="flex flex-row space-x-8 w-full mb-3 justify-around items-center text-secondary-300 font-bold">
-        <Link className="flex flex-col w-28 items-center gap-2 text-lg cursor-pointer mt-3 ml-2" href="/bands">
+        <Link className="flex flex-col w-28 items-center gap-2 text-lg cursor-pointer mt-3 ml-1 md:w-40 md:ml-20" href="/bands">
           <Image src={newLogo} alt="logo" className="w-4/5" />
         
         </Link>
-        <Link className="flex flex-col w-28 items-center gap-2 text-lg cursor-pointer mt-3 ml-360" href="/schedule"><p className={newLogo === bands ? "text" : ""}>Line-Up</p></Link>
-        <Link className="flex flex-col w-28 items-center gap-2 text-lg cursor-pointer mt-3 ml-360" href="/schedule">
+        <Link className="flex flex-col w-20 items-center gap-2 text-sm md:text-lg cursor-pointer mt-3 ml-360 md:w-80" href="/schedule"><p className={newLogo === bands ? "text" : ""}>Line-Up</p></Link>
+        <Link className="flex flex-col w-20 items-center gap-2 text-sm md:text-lg md:w-80  cursor-pointer mt-3 ml-360" href="/schedule">
           <p>Schedule</p>
         </Link>
-        <Link className="flex flex-col w-28 items-center gap-2 text-lg cursor-pointer mt-3 ml-360" href="/tickets">
+        <Link className="flex flex-col w-28 md:w-80 items-center gap-2 text-sm md:text-lg  cursor-pointer mt-3 ml-360" href="/tickets">
           <p>Get Tickets</p>
         </Link>
       </div>
