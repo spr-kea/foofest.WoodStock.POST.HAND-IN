0a07d1249f4ca862325d2c87fecb639e43635eda
Sebastian Pedersen Robles <sebastian.pedersen.2000@gmail.com>
2023-06-01 22:56:37 +0200

    footer added again

--- components/footer/footer.jsx
+++ components/footer/footer.jsx
@@ -14,7 +14,7 @@ function Footer() {
       <section className={styles.rectangle}>
       <div className={styles['my-class']}>
       
- </div>
+
       
      <div className={styles.left}>
       <Link href="/bands" className={styles.logo}> 
@@ -35,37 +35,14 @@ function Footer() {
         <Link href="/tickets">
           <p className={styles.link}>Get Tickets</p>
         </Link>
-          
+         
         </div>
-      
+       </div> 
        
   
       
-      </div>
-    </section>
     
-      <section >
-        <div className="flex flex-row space-x-8 w-full mb-3 justify-around items-center text-secondary-300 font-bold">
-          <Link className="flex flex-col w-28 items-center gap-2 text-lg cursor-pointer mt-3 ml-1 md:w-40 md:ml-20" href="/homepage">
-            <Image src={newLogo} alt="logo" className="w-4/5" />
-          
-          </Link>
-          <Link className="flex flex-col w-20 items-center gap-2 text-sm md:text-lg cursor-pointer mt-3 ml-360 md:w-80" href="/lineup"><p className={newLogo === bands ? "text" : ""}>Line-Up</p></Link>
-          
-
-          
-          <Link className="flex flex-col w-20 items-center gap-2 text-sm md:text-lg md:w-80  cursor-pointer mt-3 ml-360" href="/schedule">
-            <p>Schedule</p>
-          </Link>
-          <Link className="flex flex-col w-28 md:w-80 items-center gap-2 text-sm md:text-lg  cursor-pointer mt-3 ml-360" href="/tickets">
-            <p>Get Tickets</p>
-          </Link>
-
-
-
-        </div>
-      </section>
-    );
+    </section>);
   }
 
 export default Footer;
