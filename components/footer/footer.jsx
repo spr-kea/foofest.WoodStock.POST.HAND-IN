6c5d9fa8711ebba1111162a31a7224a9536c455e
Sebastian Pedersen Robles <sebastian.pedersen.2000@gmail.com>
2023-06-01 20:52:06 +0200

    footerjsx changed

--- components/footer/footer.jsx
+++ components/footer/footer.jsx
@@ -10,24 +10,31 @@ function Footer() {
     const [newLogo] = useState(bands);
 
     return (
-      <section >
-        <div className="flex flex-row space-x-8 w-full mb-3 justify-around items-center text-secondary-300 font-bold">
-          <Link className="flex flex-col w-28 items-center gap-2 text-lg cursor-pointer mt-3 ml-1 md:w-40 md:ml-20" href="/bands">
-            <Image src={newLogo} alt="logo" className="w-4/5" />
+      <section className={styles.rectangle}>
+      <div className={styles['my-class']}>
+      
+       <Link href="/bands" className={styles.logo}> 
+          <Image src={newLogo} alt="logo" className={styles.image} />
+        </Link>
+      
+        <div className={styles.list}>
+      <Link href="/lineup">
+          <p className={styles.link}>Line-Up</p>
+        </Link>
+        <Link href="/schedule">
+          <p className={styles.link}>Schedule</p>
+        </Link>
+        <Link href="/tickets">
+          <p className={styles.link}>Get Tickets</p>
+        </Link>
           
-          </Link>
-          <Link className="flex flex-col w-20 items-center gap-2 text-sm md:text-lg cursor-pointer mt-3 ml-360 md:w-80" href="/lineup"><p className={newLogo === bands ? "text" : ""}>Line-Up</p></Link>
-          
-
-          
-          <Link className="flex flex-col w-20 items-center gap-2 text-sm md:text-lg md:w-80  cursor-pointer mt-3 ml-360" href="/schedule">
-            <p>Schedule</p>
-          </Link>
-          <Link className="flex flex-col w-28 md:w-80 items-center gap-2 text-sm md:text-lg  cursor-pointer mt-3 ml-360" href="/tickets">
-            <p>Get Tickets</p>
-          </Link>
         </div>
-      </section>
+       
+      
+    
+      
+      </div>
+    </section>
     );
   }
 
