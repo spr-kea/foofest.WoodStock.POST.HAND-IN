7d855ff97e95848531bce192eadde25a4f1ab11a
Yani <yanivlascogmail.com>
2023-06-02 08:05:44 +0200

    test final

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: false, // or 'media' or 'class'§
   theme: {
     extend: {
       colors: {
