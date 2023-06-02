413693eda2ba815bf9312d07c5a46be7f759cb45
Ali Merkouk <63125615+Sava-Jcript@users.noreply.github.com>
2023-06-02 07:53:52 +0200

    ok gotta sleep

--- pages/tickets/bookingStep4.js
+++ pages/tickets/bookingStep4.js
@@ -1,42 +1,41 @@
 import React from "react";
 import NavBar from "../../components/nav-bar/NavBar";
-import styles from "../tickets/ticketStyles.module.css"
-import { Fireworks } from '@fireworks-js/react'
+import styles from "../tickets/ticketStyles.module.css";
+import { Fireworks } from '@fireworks-js/react';
 
-
-
-function ConfiormBooking() {
+function ConfirmBooking() {
   return (
-
-      <div >
-      <NavBar/>
+    <div>
+      <NavBar />
   
-
-      <Fireworks
-      options={{
-        rocketsPoint: {
-          min: 0,
-          max: 100
-        }
-      }}
-      style={{
-        top: 0,
-        left: 0,
-        width: '100%',
-        height: '100%',
-        position: 'fixed',
-        background: '#000'
-      }}
-    />
-    
-    <h1 className={styles.hero1}>Thank you for your purchase. You will receive your tickets via email</h1>
-   
-
+      <div style={{ position: 'relative' }}>
+        <Fireworks
+          options={{
+            rocketsPoint: {
+              min: 0,
+              max: 100
+            }
+          }}
+          style={{
+            position: 'fixed',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            zIndex: -1,
+            background: 'black'
+          }}
+        />
+        <div
+          className={styles.overlay} // Add a CSS class for overlay styles
+        >
+          <h5 className={styles.hero1}>
+            Thank you for your purchase. You will receive your tickets via email
+          </h5>
+        </div>
+      </div>
     </div>
   );
-  
 }
 
-export default ConfiormBooking;
-
-
+export default ConfirmBooking;
